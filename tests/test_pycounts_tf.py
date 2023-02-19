1dcd21400e52a4e9ee36fc36b944c9affe84001e
Tarek Ahmed Farouk <tafarouk@gmail.com>
2023-02-19 19:53:42 +0400

    test: addunit test for count_words

--- tests/test_pycounts_tf.py
+++ tests/test_pycounts_tf.py
@@ -1,1 +1,19 @@
 from pycounts_tf import pycounts_tf
+from collections import Counter
+import os
+
+def test_count_words():
+    '''Test word counting from a file.'''
+    a=os.getcwd()
+
+    expected = Counter({'insanity':1, 'is':1, 'doing': 1,
+                        'the': 1, 'same': 1, 'thing': 1,
+                        'over': 2, 'and': 2, 'expecting':1,
+                        'different': 1, 'results': 1})
+    actual = pycounts_tf.count_words("tests/einstein.txt")
+    assert actual == expected, "Einstein quote counted incorrectly!"
+
+
+
+
+
